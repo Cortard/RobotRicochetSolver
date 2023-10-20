ee4191e056e5a0d4c991fc4cbba44815cf32167e
Cortard <cfaury839@gmail.com>
2023-10-20 17:09:44 +0200

    comment constant and macro

--- Serveur/Algo/traductionCpp/solver.h
+++ Serveur/Algo/traductionCpp/solver.h
@@ -3,23 +3,23 @@
 
 #define MAX_DEPTH 32
 
-#define NORTH 0x01
-#define EAST  0x02
-#define SOUTH 0x04
-#define WEST  0x08
-#define ROBOT 0x10
+#define NORTH 0x01 // 0b00000001
+#define EAST  0x02 // 0b00000010
+#define SOUTH 0x04 // 0b00000100
+#define WEST  0x08 // 0b00001000
+#define ROBOT 0x10 // 0b00010000
 
-#define HAS_WALL(x, wall) (x & wall) // x & wall = 1 si x contient wall
-#define HAS_ROBOT(x) (x & ROBOT) // x & ROBOT = 1 si x contient ROBOT (0b00010000)
-#define SET_ROBOT(x) (x |= ROBOT) // x |= ROBOT -> x = x | ROBOT -> x = x | 0b00010000
-#define UNSET_ROBOT(x) (x &= ~ROBOT) // x &= ~ROBOT -> x = x & ~ROBOT -> x = x & 0b11101111
+#define HAS_WALL(x, wall) (x & wall) // x & wall = 1 si x contient wall (et bit à bit)
+#define HAS_ROBOT(x) (x & ROBOT) // x & ROBOT = 1 si x contient ROBOT (0b0001 0000) (et bit à bit)
+#define SET_ROBOT(x) (x |= ROBOT) // -> x = x | ROBOT -> x = x | 0b0001 0000 ( x= ou bit à bit)
+#define UNSET_ROBOT(x) (x &= ~ROBOT) // -> x = x & ~ROBOT -> x = x & 0b1110 1111 ( x= et non bit à bit)
 
-#define PACK_MOVE(robot, direction) (robot << 4 | direction) // robot << 4 | direction -> 0b00010000 | direction
-#define PACK_UNDO(robot, start, last) (robot << 16 | start << 8 | last)
-#define UNPACK_ROBOT(undo) ((undo >> 16) & 0xff)
-#define UNPACK_START(undo) ((undo >> 8) & 0xff)
-#define UNPACK_LAST(undo) (undo & 0xff)
-#define MAKE_KEY(x) (x[0] | (x[1] << 8) | (x[2] << 16) | (x[3] << 24))
+#define PACK_MOVE(robot, direction) (robot << 4 | direction) // -> 0b00[00][0000] [bit de robot (0-3)] [bit de direction]
+#define PACK_UNDO(robot, start, last) (robot << 16 | start << 8 | last) // -> 0b00[00][0000 0000][0000 0000] [bit de robot (0-3)] [bit de la case (0-255)] [bit de last (0-255)]
+#define UNPACK_ROBOT(undo) ((undo >> 16) & 0xff) // 0b[0000 0000]a [0000 0000]b [0000 0000]c -> 0b[0000 0000]a (recupere bit de robot)
+#define UNPACK_START(undo) ((undo >> 8) & 0xff) // 0b[0000 0000]a [0000 0000]b [0000 0000]c -> 0b[0000 0000]b (recupere bit de la case)
+#define UNPACK_LAST(undo) (undo & 0xff) // 0b[0000 0000]a [0000 0000]b [0000 0000]c -> 0b[0000 0000]c (recupere bit de last)
+#define MAKE_KEY(x) (x[0] | (x[1] << 8) | (x[2] << 16) | (x[3] << 24)) // -> 0b[0000 0000] [0000 0000] [0000 0000] [0000 0000] [robot 0 (goal)] [robot 1] [robot 2] [robot 3]
 
 
 
