940e6aaf20009c18ed105211739db0d203897b7f
Cortard <cfaury839@gmail.com>
2023-11-10 10:23:46 +0100

    Socket test

---
+++ Serveur/Socket/StdSocketsClient/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+#if defined (WIN32)
+#include <winsock2.h>
+typedef int socklen_t;
+#elif defined (linux)
+#include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+
+    #define INVALID_SOCKET -1
+    #define SOCKET_ERROR -1
+    #define closesocket(s) close (s)
+
+    typedef int SOCKET;
+    typedef struct sockaddr_in SOCKADDR_IN;
+    typedef struct sockaddr SOCKADDR;
+#endif
+
+#define IP "127.0.0.1"
+#define PORT 23
+
+int main() {
+    #if defined (WIN32)
+        WSADATA WSAData;
+        int erreur = WSAStartup(MAKEWORD(2,2), &WSAData);
+    #else
+        int erreur = 0;
+    #endif
+
+    if(erreur) return -1;
+
+    SOCKET sockServ;
+    sockServ = socket(AF_INET, SOCK_STREAM, 0);
+
+    SOCKADDR_IN sockAddrInter;
+    sockAddrInter.sin_addr.s_addr = inet_addr(IP);//htonl(INADDR_ANY)
+    sockAddrInter.sin_family = AF_INET;
+    sockAddrInter.sin_port = htons(PORT);
+
+    if(connect(sockServ, (SOCKADDR*)&sockAddrInter, sizeof (sockAddrInter))!= SOCKET_ERROR) {
+        printf("Connexion à %s sur le port %d\n", inet_ntoa(sockAddrInter.sin_addr), htons(sockAddrInter.sin_port));
+        size_t size=sizeof(size_t);
+        char buffer[size];
+        if(recv(sockServ, buffer, (int)size, 0) != SOCKET_ERROR) {
+            printf("Recu taille\n");
+            size_t msgSize = *(size_t*)buffer;
+            char msg[msgSize];
+            if(recv(sockServ, msg, (int)msgSize, 0) != SOCKET_ERROR) {
+                printf("Recu message\n");
+                printf("Message recu : %s\n", msg);
+                shutdown(sockServ, 2);
+            }
+            else {
+                printf("Erreur de reception message\n");
+            }
+        }
+        else {
+            printf("Erreur de reception taille\n");
+        }
+    } else {
+        printf("Impossible de se connecter\n");
+    }
+
+
+    /* On ferme la socket précédemment ouverte */
+    closesocket(sockServ);
+
+
+    #if defined (WIN32)
+        WSACleanup();
+    #endif
+
+    return 0;
+}
---
+++ Serveur/Socket/StdSocketsServ/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+#if defined (WIN32)
+    #include <winsock2.h>
+    typedef int socklen_t;
+#elif defined (linux)
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+
+    #define INVALID_SOCKET -1
+    #define SOCKET_ERROR -1
+    #define closesocket(s) close (s)
+
+    typedef int SOCKET;
+    typedef struct sockaddr_in SOCKADDR_IN;
+    typedef struct sockaddr SOCKADDR;
+#endif
+
+#define IP "127.0.0.1"
+#define PORT 23
+
+int main() {
+    #if defined (WIN32)
+        WSADATA WSAData;
+        int erreur = WSAStartup(MAKEWORD(2,2), &WSAData);
+    #else
+        int erreur = 0;
+    #endif
+
+    if(erreur) return -1;
+
+    SOCKET sockServ;
+    sockServ = socket(AF_INET, SOCK_STREAM, 0);
+
+    SOCKADDR_IN sockAddrInter;
+    sockAddrInter.sin_addr.s_addr = inet_addr(IP);//htonl(INADDR_ANY)
+    sockAddrInter.sin_family = AF_INET;
+    sockAddrInter.sin_port = htons(PORT);
+    socklen_t sockAddrInterSize = sizeof(sockAddrInter);
+
+    if( bind(sockServ,(SOCKADDR*)&sockAddrInter,sizeof(sockAddrInter)) != 0  ) {
+        std::cout << "Bind error" << std::endl;
+    }
+
+    listen(sockServ, 5);
+
+
+    while(true){
+        SOCKET clientSock=accept(sockServ, (SOCKADDR*)&sockAddrInter, &sockAddrInterSize);
+        if( clientSock == SOCKET_ERROR ) {
+            std::cout << "Connect error" << std::endl;
+            break;
+        }
+
+        std::cout << "Connect done" << std::endl;
+        std::string msg = "Hello world";
+        size_t msgSize[] = {msg.size()};
+        if(send(clientSock, (char*)msgSize, sizeof(size_t), 0)==SOCKET_ERROR){
+            std::cout << "Send size error" << std::endl;
+            closesocket(clientSock);
+            break;
+        }
+        std::cout << "Send size" << std::endl;
+
+        if(send(clientSock, msg.c_str(), (int)msgSize[0], 0)==SOCKET_ERROR){
+            std::cout << "Send message error" << std::endl;
+            closesocket(clientSock);
+            break;
+        }
+        std::cout << "Send message" << std::endl;
+
+        closesocket(clientSock);
+        break;
+    }
+
+    closesocket(sockServ);
+
+    #if defined (WIN32)
+        WSACleanup();
+    #endif
+
+    return 0;
+}
