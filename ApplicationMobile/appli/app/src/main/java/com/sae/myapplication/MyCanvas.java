107fef132763af9487676ed29c690522652c3c7d
Brounix <brounix23@gmail.com>
2023-12-01 12:22:33 +0100

    correction
    animation + methode pour corriger

--- ApplicationMobile/appli/app/src/main/java/com/sae/myapplication/MyCanvas.java
+++ ApplicationMobile/appli/app/src/main/java/com/sae/myapplication/MyCanvas.java
@@ -2,101 +2,185 @@ package com.sae.myapplication;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
+import android.os.Handler;
+
 public class MyCanvas extends View {
-    private static final int GRID_SIZE = 16;
-    private static final int CELL_SIZE = 60; // Taille de chaque cellule en pixels
 
-    private Paint gridPaint;
-    private Paint correctionPaint;
+    private int[] gridData;
+    private Paint paint;
+    private int cellSize;
 
-    private int[] correctionPath;
+    // Variables pour la correction
+    private int correctionIndex = 0;
+
+    private Handler handler;
+    private static final int MOVE_DELAY = 500; // Délai entre chaque déplacement du robot (en millisecondes)
+
+
+    private static final int NORTH = 0x01; // 0b00000001
+    private static final int EAST = 0x02;  // 0b00000010
+    private static final int SOUTH = 0x04; // 0b00000100
+    private static final int WEST = 0x08;  // 0b00001000
+    private static final int ROBOT = 0x10; // 0b00010000
+
 
     public MyCanvas(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLACK);
+        handler = new Handler();
     }
 
-    private void init() {
-        gridPaint = new Paint();
-        gridPaint.setColor(getResources().getColor(android.R.color.black));
-        gridPaint.setStrokeWidth(2);
-
-        correctionPaint = new Paint();
-        correctionPaint.setStyle(Paint.Style.FILL);
-        correctionPaint.setAlpha(150); // Set the alpha value for transparency
+    public void setGridData(int[] gridData) {
+        this.gridData = gridData;
+        invalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        drawGrid(canvas);
-        drawCorrectionPath(canvas);
-    }
 
-    private void drawGrid(Canvas canvas) {
-        int width = getWidth();
-        int height = getHeight();
-
-        // Dessine les lignes verticales
-        for (int i = 0; i <= GRID_SIZE; i++) {
-            float x = i * CELL_SIZE;
-            canvas.drawLine(x, 0, x, height, gridPaint);
+        if (gridData == null || gridData.length != 256) {
+            return;
         }
 
-        // Dessine les lignes horizontales
-        for (int i = 0; i <= GRID_SIZE; i++) {
-            float y = i * CELL_SIZE;
-            canvas.drawLine(0, y, width, y, gridPaint);
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                int value = gridData[i * 16 + j];
+                drawCell(canvas, j, i, value);
+            }
         }
+
     }
 
-    private void drawCorrectionPath(Canvas canvas) {
-        if (correctionPath != null) {
-            int cellSize = getWidth() / GRID_SIZE;
+    public void moveRobot(final int direction) {
+        final int currentRobotPosition = findRobotPosition();
+        final int currentRow = currentRobotPosition / 16;
+        final int currentCol = currentRobotPosition % 16;
 
-            for (int i = 0; i < correctionPath.length; i++) {
-                int row = i / GRID_SIZE;
-                int col = i % GRID_SIZE;
-                int correction = correctionPath[i];
+        int newRow = currentRow;
+        int newCol = currentCol;
 
-                // Extract robot position and direction from correction
-                int robotPosition = (correction >> 4) & 0x0F;
-                int direction = correction & 0x0F;
+        // Move until a wall is encountered
+        while (isValidPosition(newRow, newCol)) {
+            int newIndex = newRow * 16 + newCol;
 
-                if (robotPosition != 0) { // Only draw if robot is present in the cell
-                    RectF rect = new RectF(col * cellSize, row * cellSize,
-                            (col + 1) * cellSize, (row + 1) * cellSize);
+            if (hasWall(gridData[currentRobotPosition], direction)) {
+                break;
+            }
 
-                    correctionPaint.setColor(getColorForRobot(robotPosition));
-                    canvas.drawRect(rect, correctionPaint);
-                }
+            if (isValidPosition(newRow, newCol) && !hasRobot(gridData[newIndex])) {
+                gridData[currentRobotPosition] &= ~ROBOT; // Clear current position
+                gridData[newIndex] |= ROBOT;  // Set new position
+
+                invalidate(); // Redraw the canvas
+            } else {
+                break;
+            }
+
+            // Update row and column for the next step
+            switch (direction) {
+                case NORTH:
+                    newRow--;
+                    break;
+                case EAST:
+                    newCol++;
+                    break;
+                case SOUTH:
+                    newRow++;
+                    break;
+                case WEST:
+                    newCol--;
+                    break;
             }
         }
     }
 
-    private int getColorForRobot(int robotPosition) {
-        // Customize this method based on your color requirements
-        switch (robotPosition) {
-            case 1:
-                return getResources().getColor(android.R.color.holo_red_light);
-            case 2:
-                return getResources().getColor(android.R.color.holo_green_light);
-            case 3:
-                return getResources().getColor(android.R.color.holo_blue_light);
-            case 4:
-                return getResources().getColor(android.R.color.holo_orange_light);
-            default:
-                return getResources().getColor(android.R.color.transparent);
+    private boolean hasWall(int cellValue, int direction) {
+        return (cellValue & direction) == direction;
+    }
+
+    private boolean hasRobot(int cellValue) {
+        return (cellValue & ROBOT) == ROBOT;
+    }
+
+
+    private int findRobotPosition() {
+        for (int i = 0; i < gridData.length; i++) {
+            if ((gridData[i] & ROBOT) == ROBOT) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isValidPosition(int row, int col) {
+        return row >= 0 && row < 16 && col >= 0 && col < 16;
+    }
+
+    public void correctGrid(final int[] correctionData) {
+        correctionIndex = 0;
+
+        // Execute correction movements with a delay
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (correctionIndex < correctionData.length) {
+                    int movement = correctionData[correctionIndex];
+                    int direction = (movement & 0x10) == ROBOT ? movement & 0x0F : -1;
+
+                    if (direction != -1) {
+                        moveRobot(direction);
+                    }
+
+                    correctionIndex++;
+                    handler.postDelayed(this, MOVE_DELAY);
+                }
+            }
+        }, MOVE_DELAY);
+    }
+
+    private void drawCell(Canvas canvas, int x, int y, int value) {
+        float left = x * cellSize;
+        float top = y * cellSize;
+        float right = left + cellSize;
+        float bottom = top + cellSize;
+
+        if ((value & 0x10) == ROBOT) {
+            paint.setColor(Color.RED); // Robot
+        } else {
+            paint.setColor(Color.WHITE); // Empty cell
+        }
+
+        canvas.drawRect(left, top, right, bottom, paint);
+
+        // Draw borders based on the direction flags
+        paint.setColor(Color.BLACK);
+
+        if ((value & 0x01) == NORTH) {
+            canvas.drawLine(left, top, right, top, paint); // NORTH
+        }
+        if ((value & 0x02) == EAST) {
+            canvas.drawLine(right, top, right, bottom, paint); // EAST
+        }
+        if ((value & 0x04) == SOUTH) {
+            canvas.drawLine(left, bottom, right, bottom, paint); // SOUTH
+        }
+        if ((value & 0x08) == WEST) {
+            canvas.drawLine(left, top, left, bottom, paint); // WEST
         }
     }
 
-    public void setCorrectionPath(int[] correctionPath) {
-        this.correctionPath = correctionPath;
-        invalidate(); // Trigger redraw
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        cellSize = w / 16;
     }
 }
--- ApplicationMobile/appli/app/src/main/java/com/sae/myapplication/PictureAnswerActivity.java
+++ ApplicationMobile/appli/app/src/main/java/com/sae/myapplication/PictureAnswerActivity.java
@@ -1,32 +1,40 @@
 package com.sae.myapplication;
 
 import android.content.Intent;
-import android.graphics.Canvas;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 
 public class PictureAnswerActivity extends AppCompatActivity {
 
     ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +46,6 @@ public class PictureAnswerActivity extends AppCompatActivity {
         ImageView box2 = findViewById(R.id.footerbox2);
         ImageButton bHelp = findViewById(R.id.boutonHelp);
         ImageButton bReturn = findViewById(R.id.boutonReturn);
-        Button bCancel = findViewById(R.id.btnCancel);
-
 
 
         //getImageFromServer();
@@ -72,48 +78,58 @@ public class PictureAnswerActivity extends AppCompatActivity {
             finish();
         });
 
-        bCancel.setOnClickListener(v -> {
-            Intent intent = new Intent(this, PictureActivity.class);
-            startActivity(intent);
-            finish();
-        });
+        MyCanvas myCanvas = findViewById(R.id.canva);
 
+        int[] gridData = {9, 1, 5, 1, 3, 9, 1, 1, 1, 3, 9, 1, 1, 1, 1, 3, 8, 2, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 8, 6, 8, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 1, 0, 3, 8, 0, 0, 0, 0, 2, 12, 0, 2, 9, 0, 0, 0, 0, 4, 2, 12, 0, 0, 0, 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 3, 10, 9, 0, 0, 0, 3, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 8, 6, 8, 0, 0, 0, 0, 4, 4, 0, 0, 2, 12, 0, 0, 2, 8, 1, 0, 0, 0, 0, 2, 9, 3, 8, 0, 0, 1, 0, 0, 2, 8, 0, 4, 0, 2, 12, 2, 12, 6, 8, 0, 0, 0, 0, 0, 6, 8, 18, 9, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 4, 0, 3, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 9, 0, 2, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 8, 0, 0, 0, 2, 9, 0, 0, 0, 4, 0, 0, 0, 0, 0, 1, 0, 0, 2, 12, 2, 8, 0, 0, 16, 3, 8, 0, 0, 0, 4, 0, 0, 0, 0, 1, 2, 8, 6, 8, 0, 0, 0, 0, 0, 0, 3, 8, 0, 0, 0, 16, 2, 12, 5, 4, 4, 4, 6, 12, 4, 4, 4, 4, 6, 12, 4, 4, 6};
+        int[] correctionData = {1, 2, 4, 17, 18, 40, 33, 8, 1, 2, 1, 2, 56, 49, 56, 49, 50, 49, 8, 4, 63};
 
-        Button btnCor = findViewById(R.id.btnCor); // Replace with the actual ID
-        btnCor.setOnClickListener(new View.OnClickListener() {
+        myCanvas.setGridData(gridData);
+
+        Button btnCorrect = findViewById(R.id.btnCor);
+        btnCorrect.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startCorrection();
+            public void onClick(View v) {
+                myCanvas.correctGrid(correctionData);
             }
         });
 
+
+//        Button btnCor = findViewById(R.id.btnCor); // Replace with the actual ID
+//        btnCor.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startCorrection();
+//            }
+//        });
     }
 
-    private void startCorrection() {
-        // Assume you have a method to obtain the correction path array
-        int[] correctionPath = getCorrectionPath();
-        MyCanvas myCanvas = findViewById(R.id.canva);
-        // Set the correction path to the MyCanvas view
-        myCanvas.setCorrectionPath(correctionPath);
-    }
-
-    private int[] getCorrectionPath() {
-        // Replace this with your logic to obtain the correction path array
-        // For example, you might have a separate class or method for correction calculation
-        // that returns the correction path array.
-        // The correctionPath array structure and values should match your requirements.
-        // Dummy example:
-        int[] dummyCorrectionPath = {
-                1, 2, 4, 17, 18, 40, 33, 8, 1, 2, 1, 2, 56, 49, 56, 49, 50, 49, 8, 4, 63
-        };
-        return dummyCorrectionPath;
-    }
+//    private void startCorrection() {
+//        MyCanvas myCanvas = findViewById(R.id.canva);
+//
+//        // Assume you have a method to obtain the correction path array
+//        int[] correctionPath = getCorrectionPath();
+//
+//        // Set the correction path to the MyCanvas view
+//        myCanvas.setCorrectionPath(correctionPath);
+//    }
+//
+//    private int[] getCorrectionPath() {
+//        // Replace this with your logic to obtain the correction path array
+//        // For example, you might have a separate class or method for correction calculation
+//        // that returns the correction path array.
+//        // The correctionPath array structure and values should match your requirements.
+//        // Dummy example:
+//        int[] dummyCorrectionPath = {
+//                1, 2, 4, 17, 18, 40, 33, 8, 1, 2, 1, 2, 56, 49, 56, 49, 50, 49, 8, 4, 63
+//        };
+//        return dummyCorrectionPath;
+//    }
 
     public void fromServ() {
         Thread th = new Thread(new Runnable() {
             @Override
             public void run() {
-                String serverAddress = MainActivity.ip; // Remplacez par l'adresse IP de votre serveur
+                String serverAddress = "195.201.205.241"; // Remplacez par l'adresse IP de votre serveur
                 int serverPort = 9090; // Remplacez par le port de votre serveur
 
                 try {
@@ -124,11 +140,16 @@ public class PictureAnswerActivity extends AppCompatActivity {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(input));
                     String line = reader.readLine();    // reads a line of text
 
+
+                    //txt.setText(line);
+                    Log.d("MSG", line);
+
                     // Fermez la connexion
                     socket.close();
 
                 } catch (Exception e) {
-
+                    //txt.setText(e.toString());
+                    Log.d("ERROR", e.toString());
                 }
             }
         });
@@ -136,17 +157,20 @@ public class PictureAnswerActivity extends AppCompatActivity {
         th.start();
     }
 
-
     private void getImageFromServer(){
         // Établir une connexion socket avec le serveur
         try {
-            Socket socket = new Socket(MainActivity.ip, 9090);
+            Socket socket = new Socket(getString(R.string.IP), 8080);
+
+            // Envoyer une demande au serveur pour récupérer l'image
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            dos.writeUTF("demande_image"); // Remplacez "demande_image" par la demande réelle
 
             // Recevoir les données de l'image depuis le serveur via la connexion socket
             DataInputStream dis = new DataInputStream(socket.getInputStream());
 
             // Stocker les données de l'image dans un fichier localement
-            File imageFile = new File(getFilesDir(), "correction.jpg");
+            File imageFile = new File(getFilesDir(), "image.jpg");
             FileOutputStream fos = new FileOutputStream(imageFile);
             byte[] buffer = new byte[1024];
             int bytesRead;
